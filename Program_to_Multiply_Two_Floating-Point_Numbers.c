418931762b01ca831fe2a579893aa6ab8a19594f
Nagendra180 <106660151+Nagendra180@users.noreply.github.com>
2022-11-07 15:30:30 +0530

    #include<bits/stdc++.h> using namespace std;
    int main() {
        double a, b;
        cin >> a >> b;
        double s = (a*b);
        cout << setprecision(2) << fixed; 
        cout << s; 
    }

---
+++ Program_to_Multiply_Two_Floating-Point_Numbers.c
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    double a, b;
+    cin >> a >> b;
+    double s = (a*b);
+    cout << setprecision(2) << fixed; 
+    cout << s; 
+}
