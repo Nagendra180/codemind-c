782559530d2ffbb1b54f6b5943235211b3ee036a
Nagendra180 <106660151+Nagendra180@users.noreply.github.com>
2022-11-07 15:20:15 +0530

    #include<bits/stdc++.h> using namespace std;
    int main() {
        double a, b;
        cin >> a >> b;
        double s = (a+b)/2;
        cout << setprecision(4) << fixed; 
        cout << s; 
    }

---
+++ Find_the_average_of_two_numbers.c
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    double a, b;
+    cin >> a >> b;
+    double s = (a+b)/2;
+    cout << setprecision(4) << fixed; 
+    cout << s; 
+}
